5b6bbcdcbd7589500c51750337ffc812fe7e22a0
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-04-10 08:29:36 -0500

    Adding several Locators as bean

--- src/main/java/net/befitca/poc/aws/SecretManagerBootstrapConfiguration.java
+++ src/main/java/net/befitca/poc/aws/SecretManagerBootstrapConfiguration.java
@@ -12,20 +12,37 @@ import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.StandardEnvironment;
 
 @Configuration
 @EnableConfigurationProperties
 public class SecretManagerBootstrapConfiguration {
 
-    @Bean
-    public PropertySourceLocator myPropertySourceLocator() {
+//    @Bean
+//    public PropertySourceLocator myPropertySourceLocator() {
+//
+//        return new SMPropertySourceLocator();
+//
+//
+//    }
 
-        return new SMPropertySourceLocator();
+    @Bean
+    public PropertySourceLocator myOtherPropertySourceLocator() {
+
+        return new SMP2PropertySourceLocator();
 
 
     }
 
-    @Order(1)
+//    @Bean
+//    public PropertySourceLocator myOther2PropertySourceLocator() {
+//
+//        return new SMPEnvPropertySourceLocator();
+//
+//
+//    }
+
+    @Order(-4)
     public static class SMPropertySourceLocator implements PropertySourceLocator {
 
         @Override
@@ -33,11 +50,47 @@ public class SecretManagerBootstrapConfiguration {
             CompositePropertySource composite = new CompositePropertySource("secretmanagerpr");
 
             Map<String,Object> map = new HashMap<>();
-            map.put("com.befitca.poc.secret1", "valor que necesito bootstrap");
+            map.put("com.befitca.poc.secret1", "secretmanager");
             composite.addPropertySource(
                 new MapPropertySource("bootstrap-secret-manager", map));
 
+            return composite;
+        }
+    }
 
+    @Order(-2)
+    public static class SMP2PropertySourceLocator implements PropertySourceLocator {
+
+        @Override
+        public PropertySource<?> locate(Environment environment) {
+
+            CompositePropertySource composite = new CompositePropertySource("bootstrap-otro");
+
+            Map<String,Object> map = new HashMap<>();
+            map.put("com.befitca.poc.secret1", "bootstrapotro");
+            composite.addPropertySource(
+                new MapPropertySource("bootstrap-otro", map));
+
+            return composite;
+        }
+
+    }
+
+    @Order(-3)
+    public static class SMPEnvPropertySourceLocator implements PropertySourceLocator {
+
+        @Override
+        public PropertySource<?> locate(Environment environment) {
+
+            Map<String,Object> values = ((StandardEnvironment)environment).getSystemEnvironment();
+
+
+
+            CompositePropertySource composite = new CompositePropertySource("newenvotro");
+
+            //values.put("com.befitca.poc.secret2", "gano el boostrap ps");
+            composite.addPropertySource(
+                new MapPropertySource("newenvotro", values));
 
             return composite;
         }
